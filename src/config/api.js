dbc5cb52e676a1f5bdd403d7dc2e55014fefe535
isakkasi <83792153+isakkasi@users.noreply.github.com>
2022-10-09 22:14:12 +0300

    Add variables for prod and dev

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,3 @@
-export const baseUrl = 'http://localhost:5000';
+// export const baseUrl = 'http://localhost:5000';
 // export const baseUrl = 'http://api.buzoo.org/';
+export const baseUrl = process.env.REACT_APP_API_URL;
